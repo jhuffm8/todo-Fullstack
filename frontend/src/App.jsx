9928165cd69bd8ad18c19a4880c552ccf6a3d331
jhuffm8 <jordan.huffman85@gmail>
2024-05-23 14:32:55 -0600

    fetching data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect} from 'react'
 import './App.css'
+
 
 function App() {
 
+  const [todos, setTodos] = useState([])
+
   const textRef = useRef()
   const completeRef = useRef()
+
+  useEffect(() => {
+   const getTodos = async () => {
+    try {
+      const res = await fetch(`http://localhost:8080/api/todos`)
+      const data = await res.json()
+      console.log(data)
+      setTodos(data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  
+    }
+    getTodos()
+  }, [])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,6 +48,16 @@ function App() {
       <br /> <br />
       <button>Add Todo</button>
      </form>
+     <br /> <br />
+     {todos.map((todo) => 
+      <p style={{ textDecoration: todo.completed ? 'line-through' : ''}} 
+        key={todo.text}>
+        {todo.text}
+        </p>
+      )}
+
+
+
     </>
   )
 }
