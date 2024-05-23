afe15040cc271eb381734493acf736015f24f9ce
jhuffm8 <jordan.huffman85@gmail>
2024-05-23 14:09:11 -0600

    added todo form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
 
+  const textRef = useRef()
+  const completeRef = useRef()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(textRef.current.value)
+    console.log(completeRef.current.checked)
+
+  }
+
 
   return (
     <>
-     Hello from client
+     <h1>Todos</h1>
+     <form onSubmit={handleSubmit}>
+      <label>
+        I want to :
+        <br />
+        <input  type="text" ref={textRef}/>
+      </label>
+      <label >
+        <input type="checkbox" ref={completeRef} />
+      </label>
+      <br /> <br />
+      <button>Add Todo</button>
+     </form>
     </>
   )
 }
